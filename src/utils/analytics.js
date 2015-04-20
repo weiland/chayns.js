8ead4d6060d7418ec7c4366713f1b73181bda29d
Pascal Weiland <pasweiland@gmail.com>
2015-04-20 19:31:43 +0200

    Add analytics helper

---
+++ src/utils/analytics.js
@@ -0,0 +1,38 @@
+let types = {
+  category: 'eventCategory',
+  action: 'eventAction',
+  label: 'eventLabel',
+  value: 'eventValue'
+};
+
+export function track(info) {
+  let track = {
+    hitType: 'event'
+  };
+  info.forEach(function(key) {
+    if (types[key]) {
+      track[types[key]] = info[key];
+    }
+  });
+
+  try {
+    ga('send', track);
+  } catch (e) {
+    //log.warn('no google analytics method');
+  }
+}
+
+export function initTrack() {
+  let nodes = document.querySelector('[data-track}');
+  let i = nodes.length;
+  let callback = function() {
+    track(JSON.parse(this.getAttribute('data-track')));
+  };
+  while (i--) {
+    nodes[i].addEventListener(
+      (nodes[i].getAttribute('data-track-event') || 'click'),
+      callback,
+      false
+    );
+  }
+}
