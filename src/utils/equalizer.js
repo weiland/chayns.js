08eab884eb3dce866e510aec95ff0ec77648ad63
lengelke <le@tobit.ag>
2015-04-30 09:09:48 +0200

    cleaned up equalizer

--- src/utils/equalizer.js
+++ src/utils/equalizer.js
@@ -1,68 +1,53 @@
 /**
  * Created by LEngelke on 29.04.2015.
  */
+let dimension = {
+    WIDTH: {
+        clientDim: 'clientWidth',
+        dim: 'width'
+    },
+    HEIGHT: {
+        clientDim: 'clientHeight',
+        dim: 'height'
+    }
+};
+
 export var equalizer = (function() {
 
     function init(){
         var parents = document.querySelectorAll('[data-equalize]');
-        for(var i = 0, l = parents.length; i < l; i++){
-            let equalizeId = parents[i].dataset.equalize || '';
-            equalizeWidth(parents[i], equalizeId);
-            equalizeHeight(parents[i], equalizeId);
-            equalizeBoth(parents[i], equalizeId);
-        }
-    }
-
-    function equalizeWidth(parent, equalizeId){
-        var elements = parent.querySelectorAll(`[data-equalize-width="${equalizeId}"]`);
-        if(elements && elements.length > 0){
-            let maxWidth = 0;
-            let length = elements.length;
-            for(let i = 0; i < length; i++){
-                if(elements[i].clientWidth > maxWidth){
-                    maxWidth = elements[i].clientWidth;
-                }
+        for (var i = 0, l = parents.length; i < l; i++) {
+            let parent = parents[i];
+            let equalizeId = parent.dataset['equalize'] || '';
+            //equalize width
+            let elements = parent.querySelectorAll(`[data-equalize-width="${equalizeId}"]`);
+            if(elements.length){
+                equalize(elements, dimension.WIDTH);
             }
-            for(let i = 0, l = elements.length; i < l; i++){
-                elements[i].style.width = maxWidth + 'px';
+            //equalize height
+            elements = parent.querySelectorAll(`[data-equalize-height="${equalizeId}"]`);
+            if(elements.length){
+                equalize(elements, dimension.HEIGHT);
+            }
+            //equalize both
+            elements = parent.querySelectorAll(`[data-equalize-both="${equalizeId}"]`);
+            if(elements.length){
+                equalize(elements, dimension.WIDTH);
+                equalize(elements, dimension.HEIGHT);
             }
         }
     }
 
-    function equalizeHeight(parent, equalizeId){
-        var elements = parent.querySelectorAll(`[data-equalize-height="${equalizeId}"]`);
-        if(elements && elements.length > 0){
-            let maxHeight = 0;
-            let length = elements.length;
-            for(let i = 0; i < length; i++){
-                if(elements[i].clientHeight > maxHeight){
-                    maxHeight = elements[i].clientHeight;
-                }
-            }
-            for(let i = 0; i < length; i++){
-                elements[i].style.height = maxHeight + 'px';
+    function equalize(elements, dimension){
+        var max = 0;
+        var length = elements.length;
+        for (let i = 0; i < length; i++) {
+            if(max < elements[i][dimension.clientDim]){
+                max = elements[i][dimension.clientDim]; //find maximum dimension
             }
         }
-    }
-
-    function equalizeBoth(parent, equalizeId){
-        var elements = parent.querySelectorAll(`[data-equalize-both="${equalizeId}"]`);
-        if(elements && elements.length > 0) {
-            let maxWidth = 0;
-            let maxHeight = 0;
-            let length = elements.length;
-            for (let i = 0; i < length; i++) {
-                if (elements[i].clientWidth > maxWidth) {
-                    maxWidth = elements[i].clientWidth;
-                }
-                if (elements[i].clientHeight > maxHeight) {
-                    maxHeight = elements[i].clientHeight;
-                }
-            }
-            for (let i = 0; i < length; i++) {
-                elements[i].style.width = maxWidth + 'px';
-                elements[i].style.height = maxHeight + 'px';
-            }
+        for (let i = 0; i < length; i++) {
+            elements[i].style[dimension.dim] = max + 'px'; //set all elements to max dimension
         }
     }
 
